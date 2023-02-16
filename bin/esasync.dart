5768a55b15f29b2694021787607715a7012e22cf
margheritabagnolini <margheritabagnolini01@gmail.com>
2023-02-16 14:39:13 +0100

    Changes to be committed: 	new file:   .gitignore 	new file:   CHANGELOG.md 	new file:   analysis_options.yaml 	new file:   bin/esasync.dart 	new file:   lib/esasync.dart 	new file:   pubspec.lock 	new file:   pubspec.yaml 	new file:   test/esasync_test.dart

---
+++ bin/esasync.dart
@@ -0,0 +1,5 @@
+import 'package:esasync/esasync.dart' as esasync;
+
+void main(List<String> arguments) {
+  print('Hello world: ${esasync.calculate()}!');
+}
---
+++ test/esasync_test.dart
@@ -0,0 +1,8 @@
+import 'package:esasync/esasync.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
